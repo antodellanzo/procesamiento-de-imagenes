47a863b6148256760ec765fda00aa94b8905d317
Usuario del curso de Linux <clinux01@ws14.labo1>
2017-08-30 13:08:31 -0300

    Arreglar histograma modificado

--- histogramaModificado.m
+++ histogramaModificado.m
@@ -1,65 +1,52 @@
-function [h] = histogramaModificado(imagen, lambda)
-  h0 = histogramaDeImagen(imagen);
-  [filas, columnas] = size(imagen);
-  valorUniforme = (filas * columnas) / 256; 
-  v = ones(1, 256);
-  for i = 1 : 1 : 256
-    v(i) = valorUniforme;
-  end
-  h = (1/(1 + lambda))*h0 + ((lambda/(1+lambda))*v);
-  
-  %normalizo el histograma
-  for i = 1 : 1 : 256
-    h(i) = h(i) / (filas*columnas);
-  end
-  
-  %histograma acumulado de h
-  histoAcumulado = histogramaAcumulado(h);
-  
-  %grafico el histograma h
-  figure;
-  bar(h);
-  str = sprintf('Histograma modificado con \\lambda = %d', lambda);
-  title(str);
-  
-  %grafico el histograma acumulado de h
-  figure;
-  plot(histoAcumulado);
-  str = sprintf('Histograma acumulado modificado con \\lambda = %d', lambda);
-  title(str);
-  
-  %genero la imagen
-  y = imagen;
-  minimo = min (h);
-  for fila = 1 : 1 : filas
-    for columna = 1 : 1 : columnas
+function [h_monio] = histogramaModificado(imagenOriginal, lambda)
+    if size(imagenOriginal) > 2
+        imagen = imagenOriginal(:, :, 1);
+    else
+        imagen = imagenOriginal;
+    end
+    [filas, columnas] = size(imagen);
+    h0 = transpose(imhist(imagen));
+
+    valorUniforme = (filas * columnas) / 256; 
+    histogramaUniforme = ones(1, 256) .* valorUniforme;
+    
+    h_monio = (1/(1 + lambda))*h0 + ((lambda/(1+lambda))*histogramaUniforme);
+
+    %normalizo el histograma
+    h_monio = h_monio ./ (filas*columnas);
+
+    %histograma acumulado de h_monio
+    histoAcumulado = cumsum(h_monio);
+
+    %grafico el histograma h_monio
+    figure;
+    bar(h_monio);
+    str = sprintf('Histograma modificado con \\lambda = %d', lambda);
+    title(str);
+
+    %grafico el histograma acumulado de h_monio
+    figure;
+    plot(histoAcumulado);
+    str = sprintf('Histograma acumulado modificado con \\lambda = %d', lambda);
+    title(str);
+
+    %genero la imagen
+    y = imagen;
+    minimo = min (h_monio);
+    for fila = 1 : 1 : filas
+     for columna = 1 : 1 : columnas
       grisActual = imagen(fila, columna);
       y(fila, columna) = valorEnRango(minimo, histoAcumulado(grisActual));
-    end
-  end    
-  
-  %muestro la imagen
-  figure;
-  imshow(y);
-  str = sprintf('Imagen con \\lambda = %d', lambda);
-  title(str);
+     end
+    end    
+
+    %muestro la imagen
+    figure;
+    imshow(y);
+    str = sprintf('Imagen con \\lambda = %d', lambda);
+    title(str);
 end
 
 function [x] = valorEnRango(smin, sactual)
   x = floor( ((sactual - smin)/(1 - smin)*255) + 0.5);
-end
-
-function [histograma] = histogramaAcumulado(histogramaInput)
-  histograma = histogramaInput;
-  for index = 2 : 1 : 256
-    histograma(index) = histograma(index) + histograma(index - 1);
-  end
-end
-
-function [y] = histogramaDeImagen(imagen)
-  y = zeros(1, 256);
-  for escalaGris = 0 : 1 : 255
-    [cantidadConGrisX, x] = size( find (imagen == escalaGris) );
-    y (escalaGris + 1) = cantidadConGrisX;
-  end
 end
--- histogramaModificadoSuavizado.m
+++ histogramaModificadoSuavizado.m
@@ -1,71 +1,57 @@
-function [y] = histogramaModificadoSuavizado(imagen, lambda, gamma)
-  h0 = histogramaDeImagen(imagen);
-  [filas, columnas] = size(imagen);
-  vectorDiagonalUno = ones(1, 255);
-  vectorDiagonal = -ones(1, 256);
-  D = diag(vectorDiagonalUno, 1) + diag(vectorDiagonal);  
-  valorUniforme = (filas * columnas) / 256; 
-  hu = ones(1, 256);
-  for i = 1 : 1 : 256
-    hu(i) = valorUniforme;
-  end
-  identidad = eye(256, 256);
-  aux = h0 + lambda.*hu;
-  primerOperando = (1 + lambda).*identidad;
-  segundoOperando = gamma.*(transpose(D)*D);
-  h = inv(primerOperando + segundoOperando) * transpose(aux);
-  
-  %normalizo el histograma
-  for i = 1 : 1 : 256
-    h(i) = h(i) / (filas*columnas);
-  end
-  
-  %grafico el histograma acumulado
-  histoAcumulado = histogramaAcumulado(h);
-  plot(histoAcumulado);
-  str = sprintf('Histograma normalizado acumulado con \\lambda = %d, \\gamma = %d', lambda, gamma);
-  title(str);
-  
-  %grafico el histograma
-  figure;
-  bar(h);
-  str = sprintf('Histograma normalizado modificado suavizado con \\lambda = %d, \\gamma = %d', lambda, gamma);
-  title(str);
-  
-  %genero la imagen
-  y = imagen;
-  minimo = min (h);
-  for fila = 1 : 1 : filas
-    for columna = 1 : 1 : columnas
-      grisActual = imagen(fila, columna);
-      y(fila, columna) = valorEnRango(minimo, histoAcumulado(grisActual));
+function [y] = histogramaModificadoSuavizado(imagenOriginal, lambda, gamma)
+    if size(imagenOriginal) > 2
+        imagen = imagenOriginal(:, :, 1);
+    else
+        imagen = imagenOriginal;
     end
-  end    
-  
-  %muestro la imagen
-  figure;
-  imshow(y);
-  str = sprintf('Imagen con \\lambda = %d, \\gamma = %d', lambda, gamma);
-  title(str);
-end
+
+    h0 = transpose(imhist(imagen));
+    [filas, columnas] = size(imagen);
+
+    vectorDiagonalUno = ones(1, 255);
+    vectorDiagonal = -ones(1, 256);
+    D = diag(vectorDiagonalUno, 1) + diag(vectorDiagonal);  
+    valorUniforme = (filas * columnas) / 256; 
+    histogramaUniforme = ones(1, 256) .* valorUniforme;
+
+    identidad = eye(256, 256);
+    aux = h0 + lambda.*histogramaUniforme;
+    primerOperando = (1 + lambda).*identidad;
+    segundoOperando = gamma.*(transpose(D)*D);
+    h_monio = aux/(primerOperando + segundoOperando);
+
+    %normalizo el histograma
+    h_monio = h_monio ./ (filas*columnas);
+
+    %grafico el histograma acumulado
+    histoAcumulado = cumsum(h_monio);
+    plot(histoAcumulado);
+    str = sprintf('Histograma normalizado acumulado con \\lambda = %d, \\gamma = %d', lambda, gamma);
+    title(str);
+
+    %grafico el histograma
+    figure;
+    bar(h_monio);
+    str = sprintf('Histograma normalizado modificado suavizado con \\lambda = %d, \\gamma = %d', lambda, gamma);
+    title(str);
+
+    %genero la imagen
+    y = imagen;
+    minimo = min (h_monio);
+    for fila = 1 : 1 : filas
+        for columna = 1 : 1 : columnas
+            grisActual = imagen(fila, columna);
+            y(fila, columna) = valorEnRango(minimo, histoAcumulado(grisActual));
+        end
+    end    
+
+    %muestro la imagen
+    figure;
+    imshow(y);
+    str = sprintf('Imagen con \\lambda = %d, \\gamma = %d', lambda, gamma);
+    title(str);
+    end
 
 function [x] = valorEnRango(smin, sactual)
-  x = floor( ((sactual - smin)/(1 - smin)*255) + 0.5);
-end
-
-function [y] = histogramaDeImagen(imagen)
-  y = zeros(1, 256);
-  for escalaGris = 0 : 1 : 255
-    [cantidadConGrisX, x] = size( find (imagen == escalaGris) );
-    y (escalaGris + 1) = cantidadConGrisX;
-  end
-end
-
-function [histograma] = histogramaAcumulado(histogramaInput)
-  histograma = histogramaInput;
-  for index = 2 : 1 : 256
-    histograma(index) = histograma(index) + histograma(index - 1);
-  end
-    figure;
-    plot(histograma);
+    x = floor( ((sactual - smin)/(1 - smin)*255) + 0.5);
 end
